b8b6246523a4c71e03bbdd622595283cebf2ebdb
HuaJi66 <genshintimestamp@outlook.com>
2024-05-07 23:09:31 +0800

    edit: anything

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -12,7 +12,7 @@ import DeleteIcon from "../icons/delete.svg";
 import MaskIcon from "../icons/mask.svg";
 import PluginIcon from "../icons/plugin.svg";
 import DragIcon from "../icons/drag.svg";
-import BlogIcon from "../icons/blog.svg";
+import BlogIcon from "../icons/blog.png";
 
 import Locale from "../locales";
 
@@ -31,7 +31,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { isIOS, useMobileScreen } from "../utils";
 import dynamic from "next/dynamic";
 import { showConfirm, showToast } from "./ui-lib";
-import ImageDisplay from "@/app/components/ImageDisplay";
 
 const ChatList = dynamic(async () => (await import("./chat-list")).ChatList, {
   loading: () => null,
@@ -225,7 +224,7 @@ export function SideBar(props: { className?: string }) {
           </div>
           <div className={styles["sidebar-action"]}>
             <a href={BLOG_URL} target="_blank" rel="noopener noreferrer">
-                <ImageDisplay imageUrl={"https://huaji66.github.io/favicon.ico"}/>
+                Blog
             </a>
           </div>
         </div>
